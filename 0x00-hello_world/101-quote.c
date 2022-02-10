075374e95bdc8bf2035699fd71cdec8a21c42f6a
kadzahk <juan427lol@gmail.com>
2022-02-10 15:24:58 -0800

    correction with betty :D

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - entry point
- * return: always 0 (success)
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
