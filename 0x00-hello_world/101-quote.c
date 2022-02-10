e03f8e8811d525b04be07bcb5fe9ecf622744a33
kadzahk <juan427lol@gmail.com>
2022-02-10 15:13:58 -0800

    making the advance tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * return: always 0 (success)
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+return (1);
+}
