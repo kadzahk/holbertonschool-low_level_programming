99a966f1dd849a8b4e122679a39416e4232aba0b
kadzahk <juan427lol@gmail.com>
2022-02-11 13:37:33 -0800

    hexagecimal printed

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+char al;
+for (num = '0'; num <= '9'; num++)
+putchar(num);
+for (al = 'a'; al <= 'f'; all++)
+putchar(all);
+putchar('\n');
+return (0);
+}
