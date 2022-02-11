bca31a8a9704065b6c8d737cf2bff171691faaa1
kadzahk <juan427lol@gmail.com>
2022-02-11 12:53:16 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - print 0123456789
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf("%i", n);
+num++;
+}
+putchar('\n');
+return (0);
+}
