be02a7fe448ea6f74b42ed8343200cd2be93a9b9
kadzahk <juan427lol@gmail.com>
2022-02-15 10:39:15 -0800

    add 2 integrals in one function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - function that plus 2 integral
+ * @q: first integral
+ * @w: second integral
+ * Return: integral
+ */
+int add(int q, int w)
+{
+	return (q + w);
+}
