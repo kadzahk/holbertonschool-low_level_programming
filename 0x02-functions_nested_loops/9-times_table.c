74c78ce4b370de5c87a966488eacc0f7db86dc89
kadzahk <juan427lol@gmail.com>
2022-02-15 10:30:27 -0800

    print 9 table in row and columms

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -35,39 +34,3 @@ void times_table(void)
 		_putchar('\n');
 	}
 }
-=======
-#include "main.h"
-/**
-* times_table - function to print 9 tables
-*/
-void times_table(void)
-{
-int r, p, c;
-for (r = 0; r <= 9; r++)
-{
-for (c = 0; c <= 9; c++)
-{
-p = (r * c);
-if (c == 0)
-{
-_putchar('0' + p);
-}
-else if (p <= 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar('0' + p);
-}
-else if (p > 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar('0' + (p / 10));
-_putchar('0' + (p % 10));
-}
-}
-_putchar('\n');
-}
-}
->>>>>>> 0cc1b1e5f5ea23adc221f093634498d4a9caf1a5
