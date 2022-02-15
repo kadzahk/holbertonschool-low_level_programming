7ab09dcd6299a4a08d33855892569d74dcbe0824
kadzahk <juan427lol@gmail.com>
2022-02-15 10:28:35 -0800

    print 9 table in row and columms

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,36 +1,36 @@
 #include "main.h"
+
 /**
- * times_table - function to print 9 tables
+ * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
-int r;
-int c;
-int product;
-for (r = 0; r <= 9; r++)
-{
-for (c = 0; c <= 9; c++)
-{
-product = (r * c);
-if (c == 0)
-{
-_putchar('0' + product);
-}
-else if (product <= 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar('0' + product);
-}
-else if (product > 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar('0' + (product / 10));
-_putchar('0' + (product % 10));
-}
-}
-_putchar('\n');
-}
+	int q, w, e;
+
+	for (q = 0; q < 10; q++)
+	{
+		for (w = 0; w < 10; w++)
+		{
+			e = w * q;
+			if (w == 0)
+			{
+				_putchar(e + '0');
+			}
+
+			if (e < 10 && w != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(e + '0');
+			} else if (e >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((e / 10) + '0');
+				_putchar((e % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
