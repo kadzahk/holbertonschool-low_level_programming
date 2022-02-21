c4d5a34182c27e752b3cf1cf5091f05cda7bdf2f
kadzahk <juan427lol@gmail.com>
2022-02-21 15:10:47 -0800

    write a reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* rev_string - Reverses a string
+* @s: Input string
+* Return: String in reverse
+**/
+
+void rev_string(char *s)
+{
+	char r = s[0];
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		r = s[i];
+		s[i] = s[c];
+		s[c] = r;
+	}
+}
