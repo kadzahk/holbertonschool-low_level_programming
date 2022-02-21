13eb32c834c5234b989742230e1100f7583610b8
kadzahk <juan427lol@gmail.com>
2022-02-21 15:28:45 -0800

    print a string any count of elements in an int array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,21 +1,21 @@
+#include<stdio.h>
 #include "main.h"
 /**
- * print_array - a function that prints n elements of an array
- * @a: array name
- * @n: is the number of elements OF the array to be printed
- * Return: a and n inputs
+ *print_array - update value.
+ *@a: value to be evaluate.
+ *@n: value to be evaluate.
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int x;
 
-	for (i = 0; i < (n - 1); i++)
+	for (x = 0 ; x < n; x++)
 	{
-		printf("%d, ", a[i]);
-	}
-		if (i == (n - 1))
+		printf("%d", a[x]);
+		if (x != n - 1)
 		{
-			printf("%d", a[n - 1]);
+			printf(", ");
 		}
-			printf("\n");
+	}
+	printf("\n");
 }
