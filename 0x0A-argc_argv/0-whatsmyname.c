4bde3f825211088a1943e5dc4c117730e76087f7
kadzahk <juan427lol@gmail.com>
2022-03-04 09:05:02 -0800

    write the name of the argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{	
+	printf("%s\n", *argv);
+	return (0);
+}
