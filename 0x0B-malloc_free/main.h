ba5192b13c3d90d28aded0ef2dda8dd21539530c
kadzahk <juan427lol@gmail.com>
2022-03-07 08:49:12 -0800

    making the files

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+char *_strdup(char *str);
+void free_grid(int **grid, int height);
+
+#endif
